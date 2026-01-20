3446a72f713f3fc175419bee7f1d93fbb60353b8
Michael A <mcashton06@gmail.com>
2026-01-19 23:27:54 -0800

    Removed ability to access piece attributes and redundant occupied attribute

--- obj/square.py
+++ obj/square.py
@@ -2,8 +2,14 @@ class Square:
     def __init__(self, row, col):
         self.row = row
         self.col = col
-        self.occupied = False
         self.piece = None
     
-    def get_piece(self):
-        return self.piece.get_attribute
+    def place(self, piece):
+        if self.piece == None:
+            self.piece = piece
+        else:
+            raise ValueError("Square occupied")
+    
+    def is_empty(self):
+        # Might be unnecessary with the checker in place but we can remove later if so
+        return self.piece == None
